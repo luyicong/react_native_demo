4b9662795feb8d6d0fc18fbdfe0f0c21d6a12d93
lyc123 <980469887@qq.com>
2018-03-19 17:27:20 +0800

    。。。

--- App.js
+++ App.js
@@ -9,13 +9,19 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
+
+//导入stack导航组件
+import { StackNavigator } from 'react-navigation';
+
+import ChatScreen from './src/ChatScreen';
 
 import FlexBox from './src/common/FlexBox';
 import Header from './src/common/Header';
 import Search from './src/common/Search';
-import MyNav from './src/common/MyNav';
+// import MyNav from './src/common/MyNav';
 
 // export default class App extends Component {
 //   render() {
@@ -28,21 +34,54 @@ import MyNav from './src/common/MyNav';
 //   }
 // }
 
-export default class App extends Component {
-  render() {
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',//标题
+  };
+
+  render () {
+    const { navigate } = this.props.navigation;
     return (
-      <MyNav></MyNav>
-    );
+      <View>
+        <Text>Hello, Chat App!</Text>
+        <Button
+          onPress={() => navigate('Chat')}
+          title="Chat with Lucy"
+        />
+      </View>
+    )
   }
 }
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+const App = StackNavigator({
+  Home: { screen: HomeScreen },
+  Chat: { screen: ChatScreen },//新添加的页面
+},{
+  initialRouteName: 'Home', // 默认显示界面
+  mode: 'card',  // 页面切换模式, 左右是card(相当于iOS中的push效果), 上下是modal(相当于iOS中的modal效果)
+  navigationOptions:{
+    headerTintColor:'#000',
+    headerStyle:{
+      backgroundColor:'#ccc'
+    },
+    headerTitleStyle:{
+      fontSize:30,
+      color:'white',
+      fontWeight:'500',
+    },
+    headerBackTitleStyle:{
+      color:'red'
+    }
+    // headerTitleStyle:'#fff'
   }
 });
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     // justifyContent: 'center',
+//     // alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   }
+// });
+
+export default App
